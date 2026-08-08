b986a3352b0d3e0779da2f4cc1e205a81849c006
Santiago Pineda anaya <santiago2004nov@gmail.com>
2026-08-08 17:40:21 -0500

    Estructura base del proyecto

---
+++ cienciasII/src/main/java/udistrital/cienciasII/CienciasIiApplication.java
@@ -0,0 +1,13 @@
+package udistrital.cienciasII;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CienciasIiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CienciasIiApplication.class, args);
+	}
+
+}
---
+++ cienciasII/src/test/java/udistrital/cienciasII/CienciasIiApplicationTests.java
@@ -0,0 +1,13 @@
+package udistrital.cienciasII;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CienciasIiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
